6c45e00d49a9754d52f2e1b09172bdb3eb67adcf
Ray Krow <raykrow@Rays-MacBook-Pro.local>
2017-01-17 22:32:56 -0800

    changed header to new mobile menu and footer to just github icon

---
+++ app.js
@@ -0,0 +1,60 @@
+/**
+ * Created by raykrow on 1/17/17.
+ */
+'use strict';
+
+let krow = {
+
+    $mobileMenuIcon: null,
+    $body: null,
+    $menu: null,
+    menuActiveClass: 'rb-show-mobile-menu',
+
+
+    init: function() {
+
+        this.$mobileMenuIcon = $('#mobile-menu-icon');
+        this.$body = $('body');
+        this.$menu = $('#mobile-menu');
+
+        // Apply click listener to mobile menu icon
+        this.$mobileMenuIcon.click(this.onMenuClick);
+
+    },
+
+    onMenuClick: function(e) {
+
+        let menuIsVisible = krow.$body.hasClass(krow.menuActiveClass);
+
+        if (menuIsVisible) {
+            krow.showAll(krow.$getBodyElems());
+            krow.$menu.css('height', 0);
+        } else {
+            let ulHeight = window.innerHeight - krow.$menu.offset().top;
+            krow.$menu.css('height', ulHeight);
+            krow.hideAll(krow.$getBodyElems());
+        }
+
+        krow.$body.toggleClass(krow.menuActiveClass);
+
+    },
+
+    $getBodyElems: function() {
+        return this.$body.children().not('script, header');
+    },
+
+    hideAll: function($elems) {
+        $elems.each(function(i, elem) {
+            $(elem).hide();
+        })
+    },
+
+    showAll: function($elems) {
+        $elems.each(function(i, elem) {
+            $(elem).show();
+        })
+    }
+
+};
+
+krow.init();
